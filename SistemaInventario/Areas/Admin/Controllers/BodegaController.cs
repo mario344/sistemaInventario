02b098dce1d55a4b19078bcd290773f9ada545d6
mario344 <you@example.com>
2020-10-14 18:18:41 -0500

    Vista de Galeria hecha

---
+++ SistemaInventario/Areas/Admin/Controllers/BodegaController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SistemaInventario.AccesoDatos.repositorio.IRepositorio;
+
+namespace SistemaInventario.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BodegaController : Controller
+    {
+        private readonly IUnidadTrabajo _unidadTrabajo;
+
+        public BodegaController(IUnidadTrabajo unidadTrabajo)
+        {
+            _unidadTrabajo = unidadTrabajo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        #region API
+        [HttpGet]
+        public IActionResult ObtenerTodos() //retorna no solo la vista si no  tambien recursos en formato json,JS
+        {
+            var todos = _unidadTrabajo.Bodega.ObtenerTodos();
+            return Json(new { data = todos });
+        }
+        #endregion
+
+    }
+}
